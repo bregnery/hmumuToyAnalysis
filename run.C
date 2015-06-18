bbd02464449e9f7dca3ebf25ae0d6759b6e4892d
bregnery <bregnery@ufl.edu>
2015-06-18 05:10:06 -0400

    Fixed Jet Issue

--- run.C
+++ run.C
@@ -5,14 +5,18 @@
   gSystem->cd("../");
   gROOT->ProcessLine(".L analyzer.C++");
   
-  unsigned maxEvents = 100000000;
-  TString runPeriod("8TeV");
-  analyzer("/cms/data/store/user/jhugon/hmumu/stage1/8TeV/GluGlu_HToMM_M-125.root","ggHmumu.root",runPeriod,false,true,maxEvents,0.0846512128);
-  analyzer("/cms/data/store/user/jhugon/hmumu/stage1/8TeV/VBF_HToMM_M-125.root","vbfHmumu.root",runPeriod,false,true,maxEvents,0.006840064);
-  analyzer("/cms/data/store/user/jhugon/hmumu/stage1/WH_HToMuMu_M-125.root","wHmumu.root",runPeriod,false,true,maxEvents,0.0027143994);
-  analyzer("/cms/data/store/user/jhugon/hmumu/stage1/ZH_HToMuMu_M-125.root","zHmumu.root",runPeriod,false,true,maxEvents,0.0017098189);
+  unsigned maxEvents = 1000000;
+  TString runPeriod("13TeV");
 
-  //analyzer("/cms/data/store/user/jhugon/hmumu/stage1/8TeV/DYJetsToLL_minimal.root","DY.root",runPeriod,false,false,maxEvents,2.295515052);
+  // Scale factors are the last input variable
+  // scale factor = (cross section * SM branching ratio * Luminosity)/ Number of events generated
+  //// I had to use 14 TeV cross sections. Please change to 13 TeV and recalculate
+  //// if they become available.
+  //analyzer("/home/bregnery/13TeV/CMSSW_7_0_9/src/UserArea/UFDiMuonsAnalyzer/stage1/ggHmumu13TeV.root","ggHmumu.root",runPeriod,false,true,maxEvents,0.0846512128);
+  //analyzer("/home/bregnery/13TeV/CMSSW_7_0_9/src/UserArea/UFDiMuonsAnalyzer/stage1/VBFHmumu13TeV.root","vbfHmumu.root",runPeriod,false,true,maxEvents,0.006840064);
+  analyzer("/home/bregnery/13TeV/CMSSW_7_0_9/src/UserArea/UFDiMuonsAnalyzer/stage1/VHmumu13TeV.root","wHmumu.root",runPeriod,false,true,maxEvents,0.0027143994);
+
+  analyzer("/home/bregnery/13TeV/CMSSW_7_0_9/src/UserArea/UFDiMuonsAnalyzer/stage1/DYToLL13TeV.root","DY.root",runPeriod,false,false,maxEvents,2.295515052);
   //analyzer("/cms/data/store/user/jhugon/hmumu/stage1/8TeV/TTJets_minimal.root","TT.root",runPeriod,false,false,maxEvents,0.6413329165);
   //analyzer("/cms/data/store/user/jhugon/hmumu/stage1/8TeV/WW_minimal.root","WW.root",runPeriod,false,false,maxEvents,0.2071593204);
   //analyzer("/cms/data/store/user/jhugon/hmumu/stage1/8TeV/WZ_minimal.root","WZ.root",runPeriod,false,false,maxEvents,0.0654616994);
